7bf6242ecef627acfe25d5d1ec71f203e2684fa9
Alexamith <alex.portillo@totemtech.net>
2023-10-17 14:21:28 -0600

    Diseñando página principal

--- tcu-saludfinanciera-tcu-frontend/src/app/app.module.ts
+++ tcu-saludfinanciera-tcu-frontend/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { IndexComponent } from './components/index/index.component';
+import { PurposeComponent } from './components/purpose/purpose.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PresentationComponent,
+    IndexComponent,
+    PurposeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: IndexComponent}
+    ]),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
