2811c3fe0e283438973d7cd33c875d013cffbc79
Yordi <mvyordi2@gmail.com>
2023-11-13 12:48:35 -0600

    register final v1

--- tcu-saludfinanciera-tcu-frontend/src/app/components/register/register.component.ts
+++ tcu-saludfinanciera-tcu-frontend/src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+// Importa los módulos necesarios de Angular
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -9,42 +10,44 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   nameInvalid: boolean = false;
+  firstLastNameInvalid: boolean = false;
+  secondLastNameInvalid: boolean = false;
   emailInvalid: boolean = false;
   passwordInvalid: boolean = false;
-  errorMessage: string;
-  message: string;
+  errorMessage: boolean = false;
+  message: string = '';
 
-  constructor(private formBuilder: FormBuilder) { }
+  // Inyecta el FormBuilder en el constructor
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-    this.initializeForm();
-  }
-
-  initializeForm() {
-    this.registerForm = this.formBuilder.group({
-      name: ['', Validators.required],
+  ngOnInit(): void {
+    // Inicializa el formulario y sus validadores
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern(/[a-zA-Z ]*/)]],
+      firstLastName: ['', [Validators.required, Validators.pattern(/[a-zA-Z ]*/)]],
+      secondLastName: ['', [Validators.required, Validators.pattern(/[a-zA-Z ]*/)]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[!@#$%^&*])/)]]
     });
   }
 
+  // Función para simular el registro del usuario
   register() {
-    const nameControl = this.registerForm.get('name');
-    const emailControl = this.registerForm.get('email');
-    const passwordControl = this.registerForm.get('password');
-  
-    this.nameInvalid = nameControl ? nameControl.invalid : true;
-    this.emailInvalid = emailControl ? emailControl.invalid : true;
-    this.passwordInvalid = passwordControl ? passwordControl.invalid : true;
-  
-    if (this.nameInvalid || this.emailInvalid || this.passwordInvalid) {
-      this.errorMessage = 'Por favor, complete todos los campos correctamente.';
-      return;
+    // Simula una llamada al servicio de autenticación
+    // En este caso, simplemente imprime en la consola y muestra un mensaje
+    if (this.registerForm.valid) {
+      console.log('Simulación: Usuario registrado exitosamente');
+      // Puedes redirigir a otra página, mostrar un mensaje de éxito, etc.
+      // Dependiendo de tu aplicación.
+    } else {
+      // Marca los campos inválidos
+      this.nameInvalid = this.registerForm.get('name')?.invalid ?? false;
+      this.firstLastNameInvalid = this.registerForm.get('firstLastName')?.invalid ?? false;
+      this.secondLastNameInvalid = this.registerForm.get('secondLastName')?.invalid ?? false;
+      this.emailInvalid = this.registerForm.get('email')?.invalid ?? false;
+      this.passwordInvalid = this.registerForm.get('password')?.invalid ?? false;
+      this.errorMessage = true;
+      this.message = 'Error al registrar usuario. Por favor, revisa los campos.';
     }
-  
-    // Lógica para el registro, por ejemplo, llamada a un servicio de registro
-  
-    this.message = '¡Registro exitoso!';
   }
-  
 }
